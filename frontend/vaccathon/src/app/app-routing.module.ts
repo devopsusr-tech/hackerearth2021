00fbd8d6ca7ad9813073b68a018d1d447e6a5f25
cajlakem <emir.cajlakovic@gmail.com>
2021-02-08 00:05:07 +0100

    add angular material, bootstrap and first component

--- frontend/vaccathon/src/app/app-routing.module.ts
+++ frontend/vaccathon/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UserProfileComponent} from "./user-profile/user-profile.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/profile',
+    pathMatch: 'full'
+  }, {
+    path: 'profile',
+    component: UserProfileComponent,
+    data: {title: 'User Profile'}
+  }
+  ]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/vaccathon/src/app/app.module.ts
+++ frontend/vaccathon/src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {UserProfileComponent} from './user-profile/user-profile.component';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {BsDropdownModule} from "ngx-bootstrap/dropdown";
+import {TooltipModule} from "ngx-bootstrap/tooltip";
+import {ModalModule} from "ngx-bootstrap/modal";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    NgbModule,
+    MatButtonModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [BsDropdownModule, TooltipModule, ModalModule]
 })
-export class AppModule { }
+
+export class AppModule {
+}
