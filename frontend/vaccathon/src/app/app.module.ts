a6b899a2ab171ae4848c2e274517a3098b9d9271
cajlakem <matzenberger.verena@gmail.com>
2021-02-10 19:56:16 +0100

    add patient data to profile page

--- frontend/vaccathon/src/app/app.module.ts
+++ frontend/vaccathon/src/app/app.module.ts
@@ -6,13 +6,15 @@ import {AppComponent} from './app.component';
 import {UserProfileComponent} from './user-profile/user-profile.component';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {BsDropdownModule} from "ngx-bootstrap/dropdown";
-import {TooltipModule} from "ngx-bootstrap/tooltip";
-import {ModalModule} from "ngx-bootstrap/modal";
-import {MatButtonModule} from "@angular/material/button";
-import {HttpClientModule} from "@angular/common/http";
-import {MatTableModule} from "@angular/material/table";
-import {UsersListComponent} from "./users-list/users-list.component";
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import {TooltipModule} from 'ngx-bootstrap/tooltip';
+import {ModalModule} from 'ngx-bootstrap/modal';
+import {MatButtonModule} from '@angular/material/button';
+import {HttpClientModule} from '@angular/common/http';
+import {MatTableModule} from '@angular/material/table';
+import {UsersListComponent} from './users-list/users-list.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -27,7 +29,9 @@ import {UsersListComponent} from "./users-list/users-list.component";
     NoopAnimationsModule,
     NgbModule,
     MatButtonModule,
-    MatTableModule
+    MatTableModule,
+    MatCardModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- frontend/vaccathon/src/app/services/patient.service.ts
+++ frontend/vaccathon/src/app/services/patient.service.ts
@@ -1,7 +1,7 @@
-import {Injectable} from '@angular/core'
-import {HttpClient, HttpHeaders} from '@angular/common/http'
-import {Observable} from 'rxjs'
-import {Patient} from "../models/patient";
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Patient} from '../models/patient';
 
 @Injectable({
   providedIn: 'root'
@@ -12,15 +12,14 @@ export class PatientService {
   }
 
   public getAllPatients(): Observable<Patient[]> {
-     let test = this.httpClient.get<Patient[]>('/rest/findAllPatients');
-     console.log(test);
-     return test;
+    return this.httpClient.get<Patient[]>('/rest/findAllPatients');
   }
 
+  public getPatient(id: string): Observable<Patient> {
+    return this.httpClient.get<Patient>('/rest/findAllPatients/' + id);
+  }
 
   public getAPIVersion(): Observable<string> {
     return this.httpClient.get<string>('/rest/');
   }
-
-
 }
